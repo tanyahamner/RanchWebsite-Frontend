78fa5e56ea61e8ddd715a9b234c793d395ab5e4e
Ryan Curtis <rcurtis@devpipeline.com>
2021-09-20 11:15:28 -0600

    cleans console warnings from toastNotifications. Exports helper toast functions separately

--- src/util/toastNotifications.js
+++ src/util/toastNotifications.js
@@ -1,36 +1,37 @@
-import React from "react";
-
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import '../styles/toastNotifications.scss';
 
+import "../styles/toastNotifications.scss";
 
 toast.configure();
 
-export default function GrowlNotifications() {
-  // const app = props.app;
-  // let fileIds = app.getFileIdsFromState();
-
-
-  const errorToast = () => {
-    toast.error("Photo not added!");
-  };
-
-
-const updateSuccessfulToast = () => {
-    toast.success("User Account Updated!");
-  };
-
-const accountDeletedToast = () => {
-  toast.success(<div><i className="fas fa-check-circle"></i>User Account Deleted</div>, {hideProgressBar: true, autoClose:2987});
-  };
-
-const messageSentToast = () => {
-    toast.success("Message Sent");
-  };
+export const errorToast = () => {
+  toast.error("Photo not added!");
 };
 
+export const updateSuccessfulToast = () => {
+  toast.success("User Account Updated!");
+};
+
+export const accountDeletedToast = () => {
+  toast.success(
+    <div>
+      <i className="fas fa-check-circle"></i>User Account Deleted
+    </div>,
+    { hideProgressBar: true, autoClose: 2987 }
+  );
+};
+
+export const messageSentToast = () => {
+  toast.success("Message Sent");
+};
 
 export const successfulToast = (message) => {
-  toast.success(<div><i className="fas fa-check-circle"></i>{message}</div>, {hideProgressBar: true, autoClose:2987});
+  toast.success(
+    <div>
+      <i className="fas fa-check-circle"></i>
+      {message}
+    </div>,
+    { hideProgressBar: true, autoClose: 2987 }
+  );
 };
